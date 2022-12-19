1ff5566d3df0bff4ac849ee6e79a8e583b2a6e0b
Rhythm K C <rhythmkc@Rhythms-MacBook-Air.local>
2022-12-19 15:17:28 -0500

    fixed a spelling error

--- after/plugin/nerdTree.lua
+++ after/plugin/nerdTree.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", '<leader>nt', vim.cmd.NERDTrer)
+vim.keymap.set("n", '<leader>nt', vim.cmd.NERDTree)
 vim.keymap.set("n", '<leader>nf', vim.cmd.NERDTreeFocus)
 vim.keymap.set("n", '<leader>ntt', vim.cmd.NERDTreeToggle)
 vim.keymap.set("n", '<leader>ntv', vim.cmd.NERDTreeToggleVCS)
