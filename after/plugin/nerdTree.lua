9c3ad64eb0e8e78703258744fabf83a43d179eb7
Rhythm K C <rhythmkc@Rhythms-MacBook-Air.local>
2022-12-19 14:54:08 -0500

    added NerdTree and some of remaps for the nerdTree

---
+++ after/plugin/nerdTree.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", '<leader>nt', vim.cmd.NERDTrer)
+vim.keymap.set("n", '<leader>nf', vim.cmd.NERDTreeFocus)
+vim.keymap.set("n", '<leader>ntt', vim.cmd.NERDTreeToggle)
+vim.keymap.set("n", '<leader>ntv', vim.cmd.NERDTreeToggleVCS)
+
